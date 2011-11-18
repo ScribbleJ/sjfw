ed362be25d0d9a3a6054eb0736afc215d854a9be
Michael Moon <triffid.hunter@gmail.com>
2011-11-18 21:03:48 +1100

    whitespace change

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 #ifdef HAS_LCD
 #include "LCDKeypad.h"
 extern LCDKeypad LCDKEYPAD;
-#endif 
+#endif
 #ifdef HAS_SD
 #include "SDCard.h"
 #endif
@@ -25,44 +25,44 @@ extern LCDKeypad LCDKEYPAD;
 
 void mainloop()
 {
-  // TODO: check to see whether interleaving calls to GCODES.handlenext really gains me anything.
-  for (;;) { 
-    // Checks to see if gcodes are waiting to run and runs them if so.
-    GCODES.handlenext();
-    // Rather than interleaving, we'll just run it twice in succession, to catch instances where it
-    // wants an immediate rerun, otherwise assume it's good 'till we get back.
-    GCODES.handlenext();
-    GCODES.checkaxes();
-      
-    // Checks to see if recieve buffer has enough data to parse, and sends it to 
-    // GcodeQueue.h for processing if so.
-    HOST.scan_input();
+	// TODO: check to see whether interleaving calls to GCODES.handlenext really gains me anything.
+	for (;;) {
+		// Checks to see if gcodes are waiting to run and runs them if so.
+		GCODES.handlenext();
+		// Rather than interleaving, we'll just run it twice in succession, to catch instances where it
+		// wants an immediate rerun, otherwise assume it's good 'till we get back.
+		GCODES.handlenext();
+		GCODES.checkaxes();
 
-    // Updates temperature information; scans temperature sources
-    TEMPERATURE.update();
+		// Checks to see if recieve buffer has enough data to parse, and sends it to
+		// GcodeQueue.h for processing if so.
+		HOST.scan_input();
 
-    // Manage Eeprom operations
-    eeprom::update();
+		// Updates temperature information; scans temperature sources
+		TEMPERATURE.update();
+
+		// Manage Eeprom operations
+		eeprom::update();
 
 #ifdef HAS_LCD
-    // Update LCD, read keypresses, etc.
-    LCDKEYPAD.handleUpdates();
-#endif    
+		// Update LCD, read keypresses, etc.
+		LCDKEYPAD.handleUpdates();
+#endif
 
 #ifdef HAS_SD
-    // Manage SD card operations
-    sdcard::update();
+		// Manage SD card operations
+		sdcard::update();
 #endif
 
 #ifdef HAS_BT
-    // Look for incoming data
-    BT.scan_input();
+		// Look for incoming data
+		BT.scan_input();
 #endif
 
 #ifdef USE_MARLIN
-    Marlin::update();
+		Marlin::update();
 #endif
-  }
+	}
 
 }
 
@@ -71,44 +71,42 @@ void mainloop()
 
 int main(void)
 {
-  sei();
-  init_time();
+	sei();
+	init_time();
 
 #ifdef HAS_SD
-  sdcard::reset();
+	sdcard::reset();
 #ifdef SD_AUTORUN
-  if(sdcard::autorun())
-  {
-    HOST.write("AUTORUN GOOD\n");
+	if(sdcard::autorun())
+	{
+		HOST.write("AUTORUN GOOD\n");
 #ifdef HAS_BT
-    BT.write("AUTORUN GOOD\n");
+		BT.write("AUTORUN GOOD\n");
 #endif
-  }
-  else
-  {
-    HOST.write("AUTORUN FAIL\n");
+	}
+	else
+	{
+		HOST.write("AUTORUN FAIL\n");
 #ifdef HAS_BT
-    BT.write("AUTORUN FAIL\n");
-#endif    
-    eeprom::beginRead();
-  }
+		BT.write("AUTORUN FAIL\n");
+#endif
+		eeprom::beginRead();
+	}
 #endif
 #else
-  eeprom::beginRead();
+	eeprom::beginRead();
 #endif
 
-  HOST.write_P(PSTR("start\n"));
+	HOST.write_P(PSTR("start\n"));
 #ifdef HAS_BT
-  BT.write_P(PSTR("start\n"));
-#endif  
+	BT.write_P(PSTR("start\n"));
+#endif
 
 #ifdef USE_MARLIN
-  Marlin::init();
-#endif  
+	Marlin::init();
+#endif
 
-  mainloop();
+	mainloop();
 
-  return 0;
+	return 0;
 }
-
-
